ca1495d0bce3e721231f32c8aa0b4abff251c539
Dammy Kolawole <dammy.kolawole@gamesys.co.uk>
2017-01-26 17:56:05 +0000

    Add Time Travel Developer tool enhancer && compose all enhancers

--- web/store/index.js
+++ web/store/index.js
@@ -1,13 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reduceImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 import rootReducer from '../reducers';
 
+/* eslint-disable no-underscore-dangle */
+
+// NOTE - Add Chrome extension https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
+// Need to change this to npm module and enable only in development mode
+
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, reduceImmutableStateInvariant())
+    compose(
+      applyMiddleware(thunk, reduceImmutableStateInvariant()),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 }
