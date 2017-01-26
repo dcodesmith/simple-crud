63304d06fdcab90cc9e916dbc355b974564856f7
Dammy Kolawole <dammy.kolawole@gamesys.co.uk>
2017-01-26 17:53:06 +0000

    Add update() event onBlur of all data cells

--- web/components/EmployeeTableRow.js
+++ web/components/EmployeeTableRow.js
@@ -4,20 +4,20 @@ import { connect } from 'react-redux'
 const EmployeeTableRow = ({ employees, onDelete, onUpdate }) => {
 
   const update = (field, employee, e) => {
-    const o = {};
     const value = e.target.innerHTML;
 
-    o[field] = e.target.innerHTML;
-    onUpdate(employee, o);
+    onUpdate(employee, {
+      [field]: value
+    });
   };
 
   const employeeRows = employees.map((employee, index) => {
     return (
       <tr key={index}>
         <td contentEditable onBlur={(e) => update('firstname', employee, e)}>{employee.firstname}</td>
-        <td contentEditable>{employee.surname}</td>
-        <td contentEditable>{employee.contact_number}</td>
-        <td contentEditable>{employee.email}</td>
+        <td contentEditable onBlur={(e) => update('surname', employee, e)}>{employee.surname}</td>
+        <td contentEditable onBlur={(e) => update('contact_number', employee, e)}>{employee.contact_number}</td>
+        <td contentEditable onBlur={(e) => update('email', employee, e)}>{employee.email}</td>
         <td>
           <button className="btn-delete" onClick={() => onDelete(employee)}>&times;</button>
         </td>
